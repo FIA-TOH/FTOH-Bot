b4d7e159d293d03e3274b89db5b5153ee04d786c
Ximbeh <senhorximbinha@gmail.com>
2025-11-06 19:24:38 -0300

    feat: monaco and baku update

--- Botoh/src/room.ts
+++ Botoh/src/room.ts
@@ -57,7 +57,7 @@ export const roomPromise: Promise<any> = HaxballJS().then((HBInit: any) => {
     maxPlayers: maxPlayers,
     password: roomPassword ?? undefined,
     token:
-      process.env.HAXBALL_TOKEN ?? "thr1.AAAAAGkE31qhSFdvbMDRuA.QB_CEXNoXp4",
+      process.env.HAXBALL_TOKEN ?? "thr1.AAAAAGkNGQGk412CxlUQjA.530e7siVPkY",
     geo: getGeo(),
   });
 
