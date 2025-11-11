01eb097eef3f8d6676cdd954a4d3e4431ab86aa1
Ximbeh <senhorximbinha@gmail.com>
2025-11-11 19:54:16 -0300

    V2.6.39 fix: replays

--- Botoh/src/room.ts
+++ Botoh/src/room.ts
@@ -57,7 +57,7 @@ export const roomPromise: Promise<any> = HaxballJS().then((HBInit: any) => {
     maxPlayers: maxPlayers,
     password: roomPassword ?? undefined,
     token:
-      process.env.HAXBALL_TOKEN ?? "thr1.AAAAAGkSeDsLb92rUdkUEg.PP6pqxxPIaI",
+      process.env.HAXBALL_TOKEN ?? "thr1.AAAAAGkTuVIyT9J3IaBHbw.aRVoEJIVIUs",
     geo: getGeo(),
   });
 
