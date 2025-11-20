fbace4b1151978aa0d26642d37d78f223714e7cb
Ximbeh <senhorximbinha@gmail.com>
2025-11-20 15:32:45 -0300

    V2.7.01 feat: preparation of ped sytstem

---
+++ Botoh/src/features/scuderias/engine.ts
@@ -0,0 +1,8 @@
+export interface Engine {
+  name: string;
+  initialAcceleration: number;
+  medialAcceleration: number;
+  finalAcceleration: number;
+  topSpeedBoost: number;
+  confiability: number;
+}
---
+++ Botoh/src/features/scuderias/pitstop.ts
@@ -0,0 +1,7 @@
+export interface PitCrew {
+  name: string;
+  errorChanceReduction: number;
+  fastPitChanceBoost: number;
+  normalPitSpeedTimeBoost: number;
+  confiability: number;
+}
