e2664b94e439958b0c44491806fbd90bb45cf703
Nanoseb <Nanoseb33@gmail.com>
2026-01-22 19:11:54 -0300

    Chile Update for the Chilean GP FH

--- Botoh/src/circuits/chile/chile.ts
+++ Botoh/src/circuits/chile/chile.ts
@@ -17,11 +17,38 @@ const CHILE_INFO: CircuitInfo = {
         bounds: {
             minX: -15,
             maxX: 15,
-            minY: -280,
-            maxY: 30
+            minY: -276,
+            maxY: 31
         },
         passingDirection: Direction.RIGHT
     },
+      sectorOne: {
+    bounds: {
+      minX: -15,
+      maxX: 15,
+      minY: -276,
+      maxY: 31,
+    },
+    passingDirection: Direction.RIGHT,
+  },
+  sectorTwo: {
+    bounds: {
+      minX: -80,
+      maxX: -50,
+      minY: 1366,
+      maxY: 2088,
+    },
+    passingDirection: Direction.RIGHT,
+  },
+  sectorThree: {
+    bounds: {
+      minX: 950,
+      maxX: 980,
+      minY: -1533,
+      maxY: -377,
+    },
+    passingDirection: Direction.LEFT,
+  },
     name: "Autodromo Internacional de Codegua By Nanoseb",
     boxLine: {
         minX: -1285,
@@ -30,14 +57,14 @@ const CHILE_INFO: CircuitInfo = {
         maxY: -280
     },
     pitlaneStart: {
-        minX: -1300,
-        maxX: -1260,
+        minX: -1315,
+        maxX: -1285,
         minY: -280,
         maxY: -207
     },
     pitlaneEnd: {
-        minX: -100,
-        maxX: -70,
+        minX: -85,
+        maxX: -55,
         minY: -280,
         maxY: -207
     },
@@ -59,7 +86,7 @@ const CHILE_INFO: CircuitInfo = {
         y: chile_json.redSpawnPoints[chile_json.redSpawnPoints.length - 1][1],
     },
     BestTime: bestTimes.chile,
-    MainColor: [0x047a3d, 0xffffff, 0x047a3d],
+    MainColor: [0xffffff, 0xD10000, 0x2100C2],
     AvatarColor: 0xf30505,
     Angle: 90,
     Limit: 5,
--- Botoh/src/features/tires&pits/tires.ts
+++ Botoh/src/features/tires&pits/tires.ts
@@ -94,9 +94,9 @@ export const TYRE_DURABILITY = (limit: number | null): TireDurability => {
     // };
     // FORMULA HAXBALL 25 MIN TIRES
     return {
-      [Tires.SOFT]: 900, // ≈ 840 = 14
-      [Tires.MEDIUM]: 1080, // ≈ 980 = 16,3
-      [Tires.HARD]: 1320, // ≈ 1260 = 21
+      [Tires.SOFT]: 1200, // ≈ 840 = 14
+      [Tires.MEDIUM]: 1320, // ≈ 980 = 16,3
+      [Tires.HARD]: 2160, // ≈ 1260 = 21
       [Tires.WET]: 1200 * 0.7, // ≈ 840
       [Tires.INTER]: 1200 * 0.7, // ≈ 840
       [Tires.FLAT]: Infinity,
